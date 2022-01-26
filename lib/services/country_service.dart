fce076c04ab802e8e0b3e73402e8d6dd6448f878
Ajlani-youssef <youssefajlani@insat.u-carthage.tn>
2022-01-26 14:58:38 +0100

    add cache

--- lib/services/country_service.dart
+++ lib/services/country_service.dart
@@ -1,19 +1,36 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 import '../config.dart';
 import '../models/country.dart';
 
 class CountryService {
   static Future<List<Country>> getCountries() async {
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      List<dynamic> body = jsonDecode(response.body)["data"];
+    String filename = "countries.json";
+    var dir = await getTemporaryDirectory();
+    File file = File(dir.path + "/" + filename);
+    if (file.existsSync()) {
+      var jsonData = file.readAsStringSync();
+      List<dynamic> body = jsonDecode(jsonData)["data"];
+
       List<Country> countries = body.map((dynamic item) {
         return Country.fromJson(item);
       }).toList();
       return countries;
     } else {
-      throw jsonDecode(response.body)["error"];
+      var response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        List<dynamic> body = jsonDecode(response.body)["data"];
+        file.writeAsStringSync(response.body,
+            flush: true, mode: FileMode.write);
+        List<Country> countries = body.map((dynamic item) {
+          return Country.fromJson(item);
+        }).toList();
+        return countries;
+      } else {
+        throw jsonDecode(response.body)["error"];
+      }
     }
   }
 }

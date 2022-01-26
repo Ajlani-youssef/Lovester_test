320df06d8732b46a2da9cce054889a7a86f6d0df
Ajlani-youssef <youssefajlani@insat.u-carthage.tn>
2022-01-26 09:45:57 +0100

    splash screen

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/painting.dart';
+
+const Color mainColor = Color(0xFF8d1148);
+const Color mainColorLight = Color(0xFFf8519c);
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../config.dart';
+
+class SplashScreen extends StatelessWidget {
+  SplashScreen({Key? key}) : super(key: key) {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        print("hi");
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double defaultHeight = MediaQuery.of(context).size.height * 0.024;
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              mainColorLight,
+              mainColor,
+            ],
+            center: Alignment(-0.75, -0.75),
+            radius: 1.5,
+            stops: [0.2, 0.8],
+          ),
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: defaultHeight * 5,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: const Image(
+                image: AssetImage('assets/images/logo.png'),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.4,
+              height: 10,
+              decoration: BoxDecoration(
+                borderRadius:
+                    const BorderRadius.all(Radius.elliptical(100, 50)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    offset: const Offset(0.0, 0.0),
+                    spreadRadius: -2,
+                    blurRadius: 15,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: defaultHeight,
+            ),
+            const Text(
+              "Welcome to",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: defaultHeight,
+            ),
+            const Text(
+              "LOVESTER",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: defaultHeight * 5,
+            ),
+            SpinKitFadingCircle(
+              color: Colors.grey.withOpacity(0.5),
+              size: MediaQuery.of(context).size.width * 0.2,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
